2e751df509146d44634695bed18038164147affc
Nathan <n8flynn@outlook.com>
2023-01-23 13:03:38 -0600

    Simplified code, added in functions

--- img_2_txt/img_2_txt.py
+++ img_2_txt/img_2_txt.py
@@ -11,6 +11,19 @@ import pandas as pd
 import os
 import glob
 
+def createCoordinates(designLayout, outputName):
+    with open(outputName, 'w') as f: 
+        for k in range(66):
+            for i in range(74):
+                # Applying offset to get the coordinates to the right positon 
+                x_val = k + 0.5 
+                y_val = i + 0.5 
+
+                # If the coordinates are the middle of the physical domain, write the coordinates
+                if designLayout[k][i] == 1 and x_val > 0.5 and x_val < 65.5 and y_val > 4.5 and y_val< 73.5:
+                    f.write("%s " % (x_val))
+                    f.write("%s\n" % (y_val))
+
 # Home directory settings: 
 home = 'A:\\Research\\Training data\\run_1\\'
 ############################################################################
@@ -63,6 +76,14 @@ for image_name in os.listdir(images):
         output[5:69, 1:65] = current
         current = output 
     
+    # Filtering the image 
+    for j in range(66):
+        for i in range(74):
+            if current[i,j] >= 90:
+                current[i,j] = 0
+            else:
+                current[i,j] = 1
+
     # Forcing current to be uint8
     current0 = current[5:69, 1:65]
     current90 =  np.rot90(current0, -1)
@@ -75,26 +96,6 @@ for image_name in os.listdir(images):
         number = name.split('_')[0]
     except:
         number = name.split('_')[1]
-
-    # Saving the images for 0 degrees
-    skimage.io.imsave(filteredImages + '0_' + image_name, 255 * current0)
-    DF = pd.DataFrame(current0)
-    DF.to_csv(csvDir + '0_' + number + '.csv')
-    
-    # Saving the image for 90 degrees
-    skimage.io.imsave(filteredImages + '90_' + image_name, 255 * current90)
-    DF = pd.DataFrame(current90)
-    DF.to_csv(csvDir + '90_' + number + '.csv')
-
-    # Saving the image for 180 degrees
-    skimage.io.imsave(filteredImages + '180_' + image_name, 255 * current180)
-    DF = pd.DataFrame(current180)
-    DF.to_csv(csvDir + '180_' + number + '.csv')
-
-    # Saving the image for 270 degrees
-    skimage.io.imsave(filteredImages + '270_' + image_name, 255 * current270)
-    DF = pd.DataFrame(current270)
-    DF.to_csv(csvDir + '270_' + number + '.csv')
 
     # Splitting up the name of the file so the correct number can be saved
     outputName = image_name.strip('.jpg')
@@ -112,62 +113,16 @@ for image_name in os.listdir(images):
     current270[5:69, 1:65] = np.rot90(current270[5:69, 1:65], -3)
 
     # Transposing everything to get it centered correctly 
-    current = np.transpose(current)
+    current0 = np.transpose(current)
     current90 = np.transpose(current90)
     current180 = np.transpose(current180)
     current270 = np.transpose(current270)
 
-    # Writing the solid coordinates file for 0
-    with open(coordinates0 + outputName + '.txt', 'w') as f: 
-            for k in range(66):
-                for i in range(74):
-                    # Applying offset to get the coordinates to the right positon 
-                    x_val = k + 0.5 
-                    y_val = i + 0.5 
-
-                    # If the coordinates are the middle of the physical domain, write the coordinates
-                    if current[k][i] == 1 and x_val > 0.5 and x_val < 65.5 and y_val > 4.5 and y_val< 73.5:
-                        f.write("%s " % (x_val))
-                        f.write("%s\n" % (y_val))
-    
-    # Writing the solid coordinates file for 90
-    with open(coordinates90 + outputName + '.txt', 'w') as f: 
-            for k in range(66):
-                for i in range(74):
-                    # Applying offset to get the coordinates to the right positon 
-                    x_val = k + 0.5 
-                    y_val = i + 0.5 
-
-                    # If the coordinates are the middle of the physical domain, write the coordinates
-                    if current[k][i] == 1 and x_val > 0.5 and x_val < 65.5 and y_val > 4.5 and y_val< 73.5:
-                        f.write("%s " % (x_val))
-                        f.write("%s\n" % (y_val))
-    
-    # Writing the solid coordinates file for 180
-    with open(coordinates180 + outputName + '.txt', 'w') as f: 
-            for k in range(66):
-                for i in range(74):
-                    # Applying offset to get the coordinates to the right positon 
-                    x_val = k + 0.5 
-                    y_val = i + 0.5 
-
-                    # If the coordinates are the middle of the physical domain, write the coordinates
-                    if current[k][i] == 1 and x_val > 0.5 and x_val < 65.5 and y_val > 4.5 and y_val< 73.5:
-                        f.write("%s " % (x_val))
-                        f.write("%s\n" % (y_val))
-
-     # Writing the solid coordinates file for 270
-    with open(coordinates270 + outputName + '.txt', 'w') as f: 
-            for k in range(66):
-                for i in range(74):
-                    # Applying offset to get the coordinates to the right positon 
-                    x_val = k + 0.5 
-                    y_val = i + 0.5 
-
-                    # If the coordinates are the middle of the physical domain, write the coordinates
-                    if current[k][i] == 1 and x_val > 0.5 and x_val < 65.5 and y_val > 4.5 and y_val< 73.5:
-                        f.write("%s " % (x_val))
-                        f.write("%s\n" % (y_val))
+    # Writing the solid coordinates file for 0, 90, 180, 270
+    createCoordinates(current0, coordinates0 + outputName + '.txt')
+    createCoordinates(current90, coordinates90 + outputName + '.txt')
+    createCoordinates(current180, coordinates180 + outputName + '.txt')
+    createCoordinates(current270, coordinates270 + outputName + '.txt')
 
 # Zipping all of the files using tar and placing them into the coordinates folder
 import tarfile
