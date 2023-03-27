8c88f7f15f35e8529c0144ea7d9efe10eced47be
Nathan <n8flynn@outlook.com>
2023-03-27 16:07:08 -0500

    Added gan training data creation script

---
+++ generate_data/create_gan_training_data.py
@@ -0,0 +1,64 @@
+import numpy as np 
+import multiprocessing as mp 
+import os 
+import shutil
+
+
+def createGanData(process_files, home_dir):
+    for file in process_files:
+        try:
+            shutil.copy2(f"{home_dir}full_images\\images\\{file}.jpg", f"{home_dir}gan_images\\images\\{file}.jpg")
+        except:
+            shutil.copy2(f"{home_dir}full_images\\images\\{file}.png", f"{home_dir}gan_images\\images\\{file}.png")
+
+# Function to run conversion with multiple processes 
+def multiP_create_gan_data(home_dir, processes=20):
+    # Cleaning / working on coordinates folder 
+    try:
+        shutil.rmtree(f"{home_dir}gan_images\\")
+        print("Removing GAN images")
+    except:
+        pass
+    print("Making GAN images dir")
+    os.mkdir(f"{home_dir}gan_images\\")
+    os.mkdir(f"{home_dir}gan_images\\images\\")
+
+    # Pull in random data 
+    total_dict = {}
+    with open(f"{home_dir}\\combined_stats\\real_stats.txt", "r", encoding="UTF-16") as f:
+        lines = f.readlines()
+
+        for line in lines:
+            line = line.replace("\n", "")
+            items = line.split(", ")
+            total_dict[items[0]] = float(items[1])
+    keys = list(total_dict.keys())
+    random_files = np.random.choice(keys, 50000, replace=False)
+
+    # Writing the stats of the random data to a file 
+    with open(f"{home_dir}\\combined_stats\\gan_stats.txt", "a") as f:
+        for design in random_files:
+            temp = total_dict[design]
+            f.write(f"{design}, {temp:.4f}\n")
+
+    # Multiprocess pre-work
+    number_of_files = len(random_files)
+    number_of_files_per_process = int(number_of_files / processes)
+    
+    # Launching the processes
+    for i in range(processes):
+        offset = int(i * number_of_files_per_process)
+        try:
+            process_files = random_files[offset:(offset + number_of_files_per_process)]
+        except:
+            process_files = random_files[offset:None]
+        p = mp.Process(target=createGanData, args=(process_files, home_dir))
+        p.start()
+    
+if __name__ == "__main__":
+    # Directory the code will run out of 
+    home_dir = "A:\\Research\\Last_minute_paper_stuff\\"
+
+    # Running conversion of images to coordinates in parallel 
+    print("Running image to text conversion")
+    multiP_create_gan_data(home_dir)
