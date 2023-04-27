05e73c3109ea4319f84d5adf81df1a8af1e653fa
Nathan <n8flynn@outlook.com>
2023-04-27 11:25:41 -0500

    Cleaning

--- random_python_scripts/resize_images_to_64_x_64.py
+++
@@ -1,8 +0,0 @@
-import cv2 
-import os 
-dir_images = 'A:\\Research\\Training_data\\GAN\\run_0\\images_fid_and_is\\10K_training_images\\'
-
-for file in os.listdir(dir_images):
-    raw_image = cv2.imread(dir_images + file)
-    final_image = cv2.resize(raw_image, dsize=(64,64),  interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite(dir_images + file, final_image)
